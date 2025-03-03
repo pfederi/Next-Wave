76ca930ef604da176110eca6cec3defeb2019e09
Patrick Federi <patrick@federi.com>
2025-03-03 22:45:31 +0100

    Remove template file to fix compilation issues

--- Next Wave/API/APIConfig.template.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum APIConfig {
-    // Trage hier deinen OpenWeatherMap API-Key ein
-    // Du kannst einen kostenlosen API-Key erhalten, indem du dich auf https://openweathermap.org/api registrierst
-    // Nach der Registrierung findest du deinen API-Key unter "API keys" in deinem Account
-    static let openWeatherMapAPIKey = "YOUR_API_KEY_HERE"
-} 

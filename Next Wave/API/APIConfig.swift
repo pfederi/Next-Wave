1338f65dadb71ea2b013a1486e90810b4c97a56e
Patrick Federi <patrick@federi.com>
2025-03-03 22:48:14 +0100

    Update APIConfig.swift

--- Next Wave/API/APIConfig.swift
+++ Next Wave/API/APIConfig.swift
@@ -4,5 +4,5 @@ enum APIConfig {
     // Trage hier deinen OpenWeatherMap API-Key ein
     // Du kannst einen kostenlosen API-Key erhalten, indem du dich auf https://openweathermap.org/api registrierst
     // Nach der Registrierung findest du deinen API-Key unter "API keys" in deinem Account
-    static let openWeatherMapAPIKey = "0380ad7d17e3fb2b392b738a8eea02dc"
+    static let openWeatherMapAPIKey = "YOUR_API_KEY_HERE"
 } 

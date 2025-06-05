545309462a84bf098f54fb669157d5b2ee7f06fe
Patrick Federi <patrick@federi.com>
2025-06-05 08:40:31 +0200

    Add Watch App and Widget Extension: Implement ContentView, entitlements, and asset configurations

---
+++ Next Wave Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Next Wave Watch Watch App
+//
+//  Created by Patrick Federi on 03.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Next Wave Watch Watch App/Next_Wave_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_Wave_WatchApp.swift
+//  Next Wave Watch Watch App
+//
+//  Created by Patrick Federi on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Next_Wave_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
